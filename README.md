8f0e0f74737083fcaa5bd99415c1a329f17df759
Tim Cooper <tim@timcorp.io>
2022-07-20 14:06:13 +1000

    Start weston in YOUR supervisor
    The bones of this library was great except I wanted more control around
    supervision and how to pass configuration. Now weston is started in YOUR
    supervision tree allowing you to start multiple instances if required
    (e.g. two displays for the rpi4).

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # NervesWeston
 
-This project is part of [Very labs](https://github.com/verypossible-labs/docs/blob/master/README.md).
----
-
 Launch Weston on Nerves
+
+## Usage
+
+```elixir
+config_file = Application.app_dir(:your_app, "priv/weston.ini")
+
+{NervesWeston, tty: 1, config_file: config_file, name: :weston]
+```
 
 ## Installation
 
-Include `nerves_weston` in your dependencies, it will start as part of your supervision tree.
+Include `nerves_weston` in your dependencies referencing `github`:
 
 ```elixir
 def deps do
   [
-    {:nerves_weston, "~> 0.1.0"}
+    {:nerves_weston, github: "coop/nerves_weston"}
   ]
 end
 ```
-
--- lib/nerves_weston.ex
+++ lib/nerves_weston.ex
@@ -1,3 +1,48 @@
 defmodule NervesWeston do
+  use Supervisor
 
+  @tty 1
+  @xdg_runtime_dir "/tmp/nerves_weston"
+
+  def start_link(opts) do
+    name = opts[:name] || raise ArgumentError, "the :name option is required"
+
+    Supervisor.start_link(__MODULE__, opts, name: name)
+  end
+
+  @impl Supervisor
+  def init(opts) do
+    tty = opts[:tty] || @tty
+    extra_args = opts[:extra_args] || []
+    xdg_runtime_dir = opts[:xdg_runtime_dir] || @xdg_runtime_dir
+    args = ["--tty=#{tty}"] ++ maybe_add_config_file(opts[:config_file]) ++ extra_args
+    env = [{"XDG_RUNTIME_DIR", xdg_runtime_dir}]
+
+    setup_xdg_runtime_dir(xdg_runtime_dir)
+    setup_udev()
+
+    children = [
+      {MuonTrap.Daemon, ["weston", args, [env: env]]}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  defp maybe_add_config_file(nil), do: []
+  defp maybe_add_config_file(path), do: ["--config=#{path}"]
+
+  defp setup_xdg_runtime_dir(path) do
+    File.mkdir(path)
+    stat = File.stat!(path)
+    File.write_stat!(path, %{stat | mode: 33216})
+  end
+
+  # NOTE: One of these _require_ plugging in a device - that should be
+  # configurable.
+  defp setup_udev do
+    :os.cmd('udevd -d')
+    :os.cmd('udevadm trigger --type=subsystems --action=add')
+    :os.cmd('udevadm trigger --type=devices --action=add')
+    :os.cmd('udevadm settle --timeout=30')
+  end
 end
